685434aed49f0701af264501be60b43ac99481f4
jcherqui <joachim.cherqui@viacesi.fr>
2016-06-23 16:19:36 +0200

    Update css

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import * as config from '../js/config';
 
-const Cell = (props) => (<div className={props.type}></div>);
+const cellStyle = {
+    width: 250 / config.width,
+    height: 250 / config.height,
+};
+
+const Cell = (props) => (<div className={props.type} style={cellStyle}></div>);
 Cell.propTypes = {
     type: React.PropTypes.string,
 };
---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const width = 5;
+export const height = 5;
+export const size = [width, height];
+export const speed = 100;
