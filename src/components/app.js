0eccc0384eeb54b22eecfa65154f94ab907390d7
jcherqui <joachim.cherqui@viacesi.fr>
2016-07-11 10:57:44 +0200

    Update start & restar button

--- src/components/app.js
+++ src/components/app.js
@@ -16,21 +16,32 @@ class App extends React.Component {
             score: game.score,
             snake: game.snake,
             debug: [],
+            start: false,
         };
         this.start = this.start.bind(this);
         this.restart = this.restart.bind(this);
     }
 
-    start() {
+    componentWillMount() {
         this.tick();
     }
 
+    start() {
+        this.setState({ start: !this.state.start });
+    }
+
     restart() {
+        this.setState({ start: true });
         game.init();
     }
 
     tick() {
         setTimeout(() => {
+            if (!this.state.start) {
+                this.tick();
+                return;
+            }
+
             try {
                 const { nextMove, debug } = getNextMove(game);
                 game.nextTick(nextMove);
@@ -66,6 +77,7 @@ class App extends React.Component {
     }
 
     render() {
+        const buttonTxt = this.state.start ? 'Stop' : 'Start';
         return (
             <div>
                 {this.renderMessage()}
@@ -77,14 +89,11 @@ class App extends React.Component {
                     moves={this.state.debug.moves}
                     score={this.state.score}
                 />
-                <button onClick={this.start}>{this.props.buttonTxt}</button>
+                <button onClick={this.start}>{buttonTxt}</button>
                 <button onClick={this.restart}>Restart</button>
             </div>
         );
     }
 }
-
-App.defaultProps = { buttonTxt: 'Start' };
-App.propTypes = { buttonTxt: React.PropTypes.string };
 
 ReactDom.render(<App />, document.getElementById('app'));
